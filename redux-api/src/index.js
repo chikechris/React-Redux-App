e94e7f9b99532765156bce32d68f7cbf09a89c65
Chike Chris <cchike2017@gmail.com>
2019-08-15 16:59:35 -0400

    action creator built

--- redux-api/src/index.js
+++ redux-api/src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-// import reducer from "./reducers";
-import { createStore } from "redux";
 
-const reducer = () => {};
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 
-const store = createStore(reducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
