ad8af78d7fa2f4489de0a9c2fdda4e17857a23ed
Chike Chris <cchike2017@gmail.com>
2019-08-15 17:01:59 -0400

    worked on the redux framework

--- redux-api/src/index.js
+++ redux-api/src/index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
-import rootReducer from "./reducers";
+import reducer from "./reducers";
 import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
